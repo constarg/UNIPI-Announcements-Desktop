d6db42cbaa81d245a4bee50981960b91a5ff4e8c
Constantinos <rounnus@gmail.com>
2020-09-06 18:14:25 +0300

    Update makeLuncher.sh

--- makeLuncher.sh
+++ makeLuncher.sh
@@ -49,6 +49,7 @@ python3 main.py
 " > shellScripts/autostart.sh
 
 chmod 777 shellScripts/runUI.sh
+chmod 777 shellScripts/autostart.sh
 cp announcementsSettings.desktop /usr/share/applications/
 cp autostart/autostart.desktop /usr/share/applications/announcements.desktop
 echo "Clean up..."
